602264681b359891b90ff6ccb3fbb5914cae5a18
Ahmed Hamed <ahmed@example.com>
2026-03-10 13:04:01 +0200

    Update Kubernetes manifests and backend service

--- services/traffic-data-service/app/main.py
+++ services/traffic-data-service/app/main.py
@@ -1,9 +1,12 @@
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 import pandas as pd
 import os
 
 app = FastAPI()
+
+# CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -12,17 +15,23 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Prometheus metrics
+Instrumentator().instrument(app).expose(app)
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_PATH = os.path.join(BASE_DIR, "data", "traffic_data.csv")
+
 
 @app.get("/")
 def home():
     return {"message": "Traffic Monitoring API Running"}
 
+
 @app.get("/traffic")
 def get_traffic_data():
     df = pd.read_csv(DATA_PATH)
     return df.to_dict(orient="records")
+
 
 @app.get("/traffic/congestion")
 def get_congestion():
